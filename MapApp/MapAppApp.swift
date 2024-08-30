ab6e29de20af56a201897f1ddd71bc59a9eeeef6
vishalx06 <vishalx06@gmail.com>
2024-08-31 00:14:50 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Vishal Sharma on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
